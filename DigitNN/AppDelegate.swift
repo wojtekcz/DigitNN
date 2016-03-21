6b0858ff3ac5ea94ce8aa09cc759675e95b389f5
Wojciech Czarnowski <wcz@jatar.com.pl>
2016-03-21 11:16:36 +0100

    Initial Commit

---
+++ DigitNN/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DigitNN
+//
+//  Created by Wojciech Czarnowski on 21.03.2016.
+//  Copyright © 2016 JATAR. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DigitNN/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DigitNN
+//
+//  Created by Wojciech Czarnowski on 21.03.2016.
+//  Copyright © 2016 JATAR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
