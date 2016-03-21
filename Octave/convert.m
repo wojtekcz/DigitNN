1beb2b8499b37ff506181583a2844c24c4707359
Wojciech Czarnowski <wcz@jatar.com.pl>
2016-03-21 13:35:03 +0100

    Converting mat files to json

--- Octave/convert.m
+++ Octave/convert.m
@@ -4,13 +4,16 @@ fprintf('Loading Data ...\n')
 load('ex3data1.mat');
 m = size(X, 1);
 
-fprintf('\nLoading Saved Neural Network Parameters ...\n')
+fprintf('Loading Saved Neural Network Parameters ...\n')
 
 % Load the weights into variables Theta1 and Theta2
 load('ex3weights.mat');
 
-save("-ascii", "X.txt", "X")
-save("-ascii", "y.txt", "y")
+fprintf('Exporting data and NN parameters as JSON files ...\n')
 
-save("-ascii", "Theta1.txt", "Theta1")
-save("-ascii", "Theta2.txt", "Theta2")
+addpath('./lib/jsonlab');
+savejson('X', X, "X.json");
+savejson('y', y, "y.json");
+
+savejson('Theta1', Theta1, "Theta1.json");
+savejson('Theta2', Theta2, "Theta2.json");
